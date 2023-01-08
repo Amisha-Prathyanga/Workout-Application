ab111fe33739034887820a9fdf357d67d568d854
Amisha Prathyanga <it21132582@my.sliit.lk>
2023-01-08 21:39:11 +0530

    workout form added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 //components
 import WorkoutDetails from '../components/workoutDetails'
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -22,6 +23,7 @@ const Home = () => {
         {workouts &&
           workouts.map((workout) => (<WorkoutDetails key={workout._id} workout={workout}/>))}
       </div>
+      <WorkoutForm/>
     </div>
   );
 };
